29f3297d97ec452622047fcf9177d095fec88420
Your Name <you@example.com>
2022-04-20 21:26:42 +0300

    урок по видео 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//
+        System.out.println ("hello whorld");
+        int dog = 7;
+        byte cat = 1;
+        short mouse = 3;
+        long elefant = 145L;
+        double sugarWeight = 4.5;
+        float saltWeight = 1.5f;
+        boolean dogIsAdult = dog > 10;
+        char bar = 35;
+        System.out.println(dogIsAdult);
+        // Задание номер 2
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double weightOfAllBoxers = boxer1 + boxer2;
+        double weightDifference = boxer2 - boxer1;
+        System.out.println("Общий вес боксеров" + weightOfAllBoxers);
+        System.out.println("Разница в весе боксеров" + weightDifference);
+        //
+
+
+
+    }
+}
